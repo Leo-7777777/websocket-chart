707ef530e15f99df05634bf8d2d1a6f376252552
LaOngDaoxing <18731197300@163.com>
2021-03-26 10:22:38 +0800

    主程序版本_单条件、多条件websocket

--- src/main/java/com/zxw/controller/UserController.java
+++ src/main/java/com/zxw/controller/UserController.java
@@ -7,9 +7,8 @@ import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-
 /**
  *
  * @Description:
@@ -41,6 +40,26 @@ public class UserController {
         return userService.dealUserChartDTOByGroupCode(groupCode);
     }
     /**
+     * webUrll测试http://localhost:8090/addAndQueryByGroupCode
+     * Description: 新增一条用户数据；并返回拼装的用户数据（根据分组编码，列表查询用户表）
+     *
+     * @Param user:
+     * @return: com.zxw.model.User
+     * Author: ljx
+     * Date: 2021/3/24 0024 上午 10:14
+     */
+    @PostMapping("/addAndQueryByGroupCode")
+    public UserDO addAndQueryByGroupCode(UserDO user) {
+        // 保存数据
+        UserDO result = userService.addUser(user);
+        String groupCode=user.getGroupCode();
+        // 拼装的用户数据（根据分组编码，列表查询用户表）
+        UserChartDTO userChartDTO = userService.dealUserChartDTOByGroupCode(groupCode);
+        // 通知前端
+        UserWebSocket.sendMessage(user.getGroupCode(), JSON.toJSONString(userChartDTO));
+        return result;
+    }
+    /**
      * Title: selectUserChartDTOByParams
      * Description: 根据分组编码、前台页面条件，列表查询用户表
      * @Param groupCode:
@@ -54,21 +73,28 @@ public class UserController {
         params.put("groupCode",groupCode);
         return userService.dealUserChartDTOByParams(params);
     }
+
     /**
-     * webUrll测试http://localhost:8090/add
-     * Description: 新增一条用户数据；并返回拼装的用户数据
+     * webUrll测试http://localhost:8090/addAndQueryByGroupCode
+     * Description: 新增一条用户数据；并返回拼装的用户数据（根据分组编码、前台页面条件，列表查询用户表）
      *
      * @Param user:
      * @return: com.zxw.model.User
      * Author: ljx
      * Date: 2021/3/24 0024 上午 10:14
      */
-    @PostMapping("/add")
-    public UserDO add(UserDO user) {
-        //保存数据
+    @PostMapping("/addAndQueryByParams")
+    public UserDO addAndQueryByParams(UserDO user) {
+        // 保存数据
         UserDO result = userService.addUser(user);
-        //拼装数据DTO通知前端
-        UserChartDTO userChartDTO = userService.dealUserChartDTOByGroupCode(user.getGroupCode());
+        String groupCode=user.getGroupCode();
+        String emailParam=user.getEmail();
+        Map<String, Object> params=new HashMap<String, Object>();
+        params.put("groupCode",groupCode);
+        params.put("emailParam",emailParam);
+        // 拼装的用户数据（根据分组编码，列表查询用户表）
+        UserChartDTO userChartDTO = userService.dealUserChartDTOByParams(params);
+        // 通知前端
         UserWebSocket.sendMessage(user.getGroupCode(), JSON.toJSONString(userChartDTO));
         return result;
     }
