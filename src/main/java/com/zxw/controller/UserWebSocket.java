2a222eba3910268bcccf8461a70c9f196d31de0e
LaOngDaoxing <18731197300@163.com>
2021-04-08 10:14:53 +0800

    版本master // 给用户的所有终端发送数据消息

--- src/main/java/com/zxw/controller/UserWebSocket.java
+++ src/main/java/com/zxw/controller/UserWebSocket.java
@@ -32,7 +32,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserWebSocket {
     private static final Logger logger = LoggerFactory.getLogger(UserWebSocket.class);
     /**
-     * 存储 websocket session，以记录每个用户下多个终端的连接
+     * 存储 websocket session等，以记录每个用户下多个终端（PC、pad、phone）的连接
      */
     public static final Map<String, List<Session>> ONLINE_USER_SESSIONS = new ConcurrentHashMap<>();
     /*######################## 一、根据分组编码，接收 消息(用户信息)的 websocket服务器端 ########################*/
@@ -51,10 +51,11 @@ public class UserWebSocket {
         // /userws/dkh?emailWsParam=1
         String uri = session.getRequestURI().toString();
         List<Session> list = ONLINE_USER_SESSIONS.get(groupCode);
+        // 如果该用户当前是第一次连接/没有在别的终端登录
         if (null == list) {
             list = new ArrayList<>();
         }
-
+        // 如果该用户当前不是第一次连接/已经在别的终端登录
         if (!list.contains(session)) {
             list.add(session);
         }
@@ -68,8 +69,8 @@ public class UserWebSocket {
 
     @OnClose
     public void onClose(@PathParam("groupCode") String groupCode, Session session) {
-        //当前的Session 移除
         List<Session> list = ONLINE_USER_SESSIONS.get(groupCode);
+        // 移除该用户的websocket session记录
         list.remove(session);
         try {
             session.close();
@@ -110,8 +111,8 @@ public class UserWebSocket {
         }
     }
     /**
-     * Title: sendMessage
-     * Description: 根据分组编码，向客户端发送 消息(用户信息)
+     * Description: 这个根据业务情况详细设计
+     * @CodeSteps： 根据分组编码，向客户端发送 消息(用户信息)
      * @Param key:
      * @Param message:
      * @return: void
@@ -120,6 +121,7 @@ public class UserWebSocket {
      */
     public static void sendMessage(String key, String message) {
         List<Session> list = ONLINE_USER_SESSIONS.get(key);
+        // 给用户的所有终端发送数据消息
         list.stream().forEach(se -> {
             if(se.isOpen()){
                 sendMessage(se, message);
