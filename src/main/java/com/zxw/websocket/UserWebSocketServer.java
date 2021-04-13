dcacadb4b5452e3f1c48648bf8c590be545586b3
LaOngDaoxing <18731197300@163.com>
2021-04-13 13:36:17 +0800

    版本2.0  // 接收 消息(用户信息)的 websocket服务端

--- src/main/java/com/zxw/websocket/UserWebSocketServer.java
+++ src/main/java/com/zxw/websocket/UserWebSocketServer.java
@@ -1,9 +1,8 @@
-package com.zxw.controller;
+package com.zxw.websocket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
@@ -16,11 +15,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
-* @Description: 接收 消息(用户信息)的 websocket服务器端
+* @Description: 接收 消息(用户信息)的 websocket服务端
 * @FR功能需求：
 * @ImportJar:
 * @ApiGrammer规则：
-    注解@ServerEndpoint 是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端。注解的值将被用于监听用户连接的终端访问URL地址。
+    注解@ServerEndpoint 是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务端。注解的值将被用于监听用户连接的终端访问URL地址。
 * @Remark:
 * @CodeBug解决:
 * @date 2021年3月24日 下午1:36:00
@@ -29,15 +28,15 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @ServerEndpoint("/userws/{userId}")
 @Component
-public class UserWebSocket {
-    private static final Logger logger = LoggerFactory.getLogger(UserWebSocket.class);
+public class UserWebSocketServer {
+    private static final Logger logger = LoggerFactory.getLogger(UserWebSocketServer.class);
     /**
      * 存储 websocket session等，以记录每个用户下多个终端【PC（不同浏览器登陆，产生的sessionid不同）、pad、phone】的连接
      */
     public static final Map<String, List<Session>> ONLINE_USER_SESSIONS = new ConcurrentHashMap<>();
-    /*######################## 一、根据用户id，接收 消息(用户信息)的 websocket服务器端 ########################*/
+    /*######################## 一、根据用户id，接收 消息(用户信息)的 websocket服务端 ########################*/
     /**
-     * 当WebSocket客户端与服务器建立连接并完成握手后，前台会回调ws.onopen；后台调用@OnOpen注解的方法。
+     * 当前台用户终端【浏览器】页面，使用js WebSocket；与服务器建立连接并完成握手后，前台会回调ws.onopen；后台调用@OnOpen注解的方法。
      * @param userId
      * @param session
      */
@@ -64,7 +63,7 @@ public class UserWebSocket {
 
     @OnMessage
     public void onMessage(@PathParam("userId") String userId, String message) {
-        System.out.println(userId + "客户端ws.send发送的消息（或心跳信息）：" + message);
+        System.out.println(userId + "前台用户终端【浏览器】页面，ws.send发送的消息（或心跳信息）：" + message);
     }
 
     @OnClose
@@ -88,11 +87,11 @@ public class UserWebSocket {
         }
         System.out.println("Throwable msg " + throwable.getMessage());
     }
-    /*######################## 二、根据用户id，发送 消息(用户信息)的 websocket服务器端 工具方法########################*/
+    /*######################## 二、根据用户id，发送 消息(用户信息)的 websocket服务端 工具方法########################*/
     /**
      * @Author Zhouxw
      * @Date 2020/09/21 13:19
-     * @Description 向客户端发送 消息
+     * @Description 向前台用户终端【浏览器】页面，发送 消息(用户信息)
      * @Param [session, message]
      * @Return void
      */
@@ -112,12 +111,10 @@ public class UserWebSocket {
     }
     /**
      * Description: 这个根据业务情况详细设计
-     * @CodeSteps： 根据用户id，向客户端发送 消息(用户信息)
+     * @CodeSteps： 根据用户id，向前台用户终端【浏览器】页面，发送 消息(用户信息)
      * @Param key:
      * @Param message:
      * @return: void
-     * Author: ljx
-     * Date: 2021/3/24 0024 下午 3:35
      */
     public static void sendMessage(String key, String message) {
         List<Session> list = ONLINE_USER_SESSIONS.get(key);
