4dc9f20a74d98227fa2a98235b69874d89e8717c
LaOngDaoxing <18731197300@163.com>
2021-04-13 10:34:31 +0800

    版本master                 //数据库触发器的外部程序或接口

--- src/main/java/com/zxw/websocket/UserWebSocketClient.java
+++ src/main/java/com/zxw/websocket/UserWebSocketClient.java
@@ -1,18 +1,13 @@
 package com.zxw.websocket;
-
 /**
  * @author ljx
  * @Description: 数据库触发器的外部程序或接口
  * @FR功能需求：
-前台用户终端【浏览器】页面，使用js WebSocket；
-通过后台服务器的websocket服务器端|ServerEndpoint，维护Java服务器会话、用户终端的Sessions；
-与在后台服务器的websocket客户端|Java服务器|ClientEndpoint，进行通信。
-
-
-写一个ExternalProgramOrInterfaceOfDbTrigger.java，当运行时与websocket建立连接，并将参数发送到此websocket
-
-将ExternalProgramOrInterfaceOfDbTrigger.java，构建成jar程序
-                数据库建立insert触发器，当插入数据时调用外部jar程序
+        前台用户终端【浏览器】页面，使用js WebSocket；
+        通过后台服务器的websocket服务器端|ServerEndpoint即UserWebSocketServer.java，维护Java服务器会话、用户终端的Sessions；
+        与在后台服务器的websocket客户端|Java服务器|ClientEndpoint即UserWebSocketClient.java，进行双向通信；
+        将UserWebSocketClient.java，构建成外部程序jar包；
+        数据库建立insert触发器，当插入数据时调用外部程序jar包。
  * @ImportJar:
  * @ApiGrammer规则：
  * @Remark:
