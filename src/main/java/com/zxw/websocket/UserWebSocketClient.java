b37e1b2be7d9b4e905140a36655a57ca43985aad
LaOngDaoxing <18731197300@163.com>
2021-04-13 18:50:26 +0800

    版本master //websocket客户端，发送消息到websocket服务端

--- src/main/java/com/zxw/websocket/UserWebSocketClient.java
+++ src/main/java/com/zxw/websocket/UserWebSocketClient.java
@@ -50,46 +50,4 @@ public class UserWebSocketClient {
         public void onClose(){
                 System.out.println("Websocket closed");
         }
-        /*######################## 二、发送 消息的 websocket客户端 工具方法########################*/
-        /**
-         * websocket服务端地址
-         */
-        private static String uri = "ws://192.168.21.21:8090/websocket-chart/userws/"+"dkh";
-        private static Session session;
-        public static  void main(String[] args) {
-                // websocket客户端，发送消息到websocket服务端
-                sendMessageToUserWebSocketServer();
-        }
-        /**
-         * @Description: websocket客户端，连接websocket服务端
-         * @return:
-         */
-        protected static void connectUserWebSocketServer(){
-                WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-                System.out.println("Connecting to" + uri);
-                try {
-                        session = container.connectToServer(UserWebSocketClient.class, URI.create(uri));
-                } catch (DeploymentException e) {
-                        e.printStackTrace();
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-        }
-        /**
-         * @Description: websocket客户端，发送消息到websocket服务端
-         * @Param args:
-         * @return:
-         */
-        public static void sendMessageToUserWebSocketServer(){
-                // websocket客户端，连接websocket服务端
-                connectUserWebSocketServer();
-                // 发送到websocket服务端，的消息“数据库表，有数据变动”
-                String message = "dbTableCurrentHaveUpdate";
-                try {
-                        session.getBasicRemote().sendText(ConstantUtil.TO_WEBSOCKET_OF_CLIENT_TYPE2+"_"+message);
-                        session.close();
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-        }
 }
