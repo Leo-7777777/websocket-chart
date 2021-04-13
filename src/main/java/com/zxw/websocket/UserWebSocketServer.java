20ce3c8ffaa92a192b405c09e25c103988150ac1
LaOngDaoxing <18731197300@163.com>
2021-04-13 15:56:08 +0800

    版本master //通知前端，向前台发送消息

--- src/main/java/com/zxw/websocket/UserWebSocketServer.java
+++ src/main/java/com/zxw/websocket/UserWebSocketServer.java
@@ -71,7 +71,7 @@ public class UserWebSocketServer {
             System.out.println(userId + "前台用户终端【浏览器】页面，ws.send发送的消息（或心跳信息）：" + message);
             // --接收并处理消息
 
-            // --向前台发送消息
+            // --通知前端，向前台发送消息
 
         }
         // 后台服务器的websocket客户端|ClientEndpoint|Java服务器，UserWebSocketClient.java发送的消息（或心跳信息）
